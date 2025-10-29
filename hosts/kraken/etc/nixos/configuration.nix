8e1ecf993b1f7c400b6c4e7c0a53ed0f826e7515
FKouhai <frandres00@gmail.com>
2025-10-29 20:46:26 +0100

    feat: add chaotic-nyx substituters

--- hosts/kraken/etc/nixos/configuration.nix
+++ hosts/kraken/etc/nixos/configuration.nix
@@ -26,18 +26,30 @@ in
   ];
 
   nix = {
-    settings.experimental-features = [
-      "nix-command"
-      "flakes"
-    ];
+    settings = {
+      substituters = [
+        "https://nix-community.cachix.org/"
+        "https://chaotic-nyx.cachix.org/"
+        "https://cache.nixos.org/"
+      ];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8"
+      ];
+
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+      trusted-users = [
+        "root"
+        "franky"
+        "@wheel"
+      ];
+    };
 
     optimise.automatic = true;
-
-    settings.trusted-users = [
-      "root"
-      "franky"
-      "@wheel"
-    ];
 
     gc = {
       automatic = true;
