52a1057abfc5102f690b5ca897dc6dd184c58a0d
FKouhai <frandres00@gmail.com>
2025-11-23 17:32:44 +0100

    chore: refactor config kitty

---
+++ terminals/kitty.nix
@@ -0,0 +1,29 @@
+{
+  lib,
+  config,
+  ...
+}:
+{
+  options = {
+    kitty.enable = lib.mkEnableOption "Enable kitty module";
+  };
+  config = lib.mkIf config.kitty.enable {
+    programs.kitty = {
+      enable = true;
+      settings = {
+        font_family = "Hack Nerd Font";
+        bold_font = "auto";
+        italic_font = "auto";
+        bold_italic_font = "auto";
+        enable_audio_bell = false;
+        scrollback_lines = -1;
+        tab_bar_edge = "top";
+        allow_remote_control = "yes";
+      };
+      shellIntegration = {
+        enableZshIntegration = true;
+      };
+      themeFile = "kanagawa";
+    };
+  };
+}
