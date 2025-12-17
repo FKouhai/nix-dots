794bf50d689fbe9b576bc09432bb6390ed40a648
FKouhai <frandres00@gmail.com>
2025-12-17 21:36:03 +0100

    feat: add caelestia shell configuration

--- hyprland/default.nix
+++ hyprland/default.nix
@@ -6,6 +6,7 @@
 {
   imports = [
     ./hypr.nix
+    ./caelestia.nix
     ./hyprpanel.nix
     ./hyprlock.nix
     ./hyprpaper.nix
@@ -17,7 +18,8 @@
   };
   config = lib.mkIf config.hyprland.enable {
     hypr.enable = lib.mkDefault true;
-    hyprpanel.enable = lib.mkDefault true;
+    caelestia.enable = lib.mkDefault true;
+    hyprpanel.enable = lib.mkDefault false;
     hyprlock.enable = lib.mkDefault true;
     hyprpaper.enable = lib.mkDefault true;
     wlogout.enable = lib.mkDefault true;
--- hyprland/hypr.nix
+++ hyprland/hypr.nix
@@ -86,6 +86,7 @@
           exec-once = [
             "dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
             "add_record_player"
+            "caelestia wallpaper --file ${vars.wallpaper}"
             "wl-paste --watch cliphist store &"
           ];
         }
