0c19a39ab9cc9e7a0c0720a542fade2350f1b89f
FKouhai <frandres00@gmail.com>
2026-02-18 20:49:42 +0100

    chore(tmux): update config
    * use ukiyo plugin
    * configure resurrect
    * configure tmux-fzf
    * configure continuum

--- devtooling/tmux/default.nix
+++ devtooling/tmux/default.nix
@@ -14,12 +14,42 @@
       keyMode = "vi";
       shortcut = "a";
       plugins = with pkgs.tmuxPlugins; [
-        resurrect
-        tmux-fzf
-        continuum
+        {
+          plugin = resurrect;
+          extraConfig = ''
+            set -g @resurrect-strategy-vim "session"
+            set -g @resurrect-strategy-nvim "session"
+            set -g @resurrect-capture-pane-contents "on"
+          '';
+        }
+        {
+          plugin = tmux-fzf;
+          extraConfig = ''
+            set -g @plugin sainnhe/tmux-fzf
+          '';
+        }
+        {
+          plugin = continuum;
+          extraConfig = ''
+            set -g @continuum-restore "on"
+            set -g @continuum-boot "on"
+            set -g @continuum-save-interval "10"
+          '';
+        }
         better-mouse-mode
         harpoon
-        dotbar
+        {
+          plugin = ukiyo;
+          extraConfig = ''
+            set -g @ukiyo-theme "kanagawa/wave"
+            set -g @ukiyo-playerctl-format "►  {{ artist }} - {{ title }}"
+            set -g @ukiyo-ignore-window-colors true
+            set -g @ukiyo-refresh-rate 10
+            set -g @ukiyo-show-battery false
+            set -g @ukiyo-show-powerline true
+            set -g @ukiyo-refresh-rate 10
+          '';
+        }
       ];
       extraConfig = ''
         bind-key h select-pane -L
@@ -30,28 +60,6 @@
         set -g mouse "on"
         set -g allow-passthrough on
         set-option -g status-position top
-        set -g @plugin 'vaaleyard/tmux-dotbar'
-        set -g @tmux-dotbar-bg "#1e1e2e"
-        set -g @tmux-dotbar-fg "#585b70"
-        set -g @tmux-dotbar-fg-current "#cdd6f4"
-        set -g @tmux-dotbar-fg-session "#9399b2"
-        set -g @tmux-dotbar-fg-prefix "#cba6f7"
-        set -g @tmux-dotbar-justify "absolute-centre"
-        set -g @tmux-dotbar-left "true"
-        set -g @tmux-dotbar-status-left "#S" # see code
-        set -g @tmux-dotbar-right "false"
-        set -g @tmux-dotbar-status-right "%H:%M" # see code
-        set -g @tmux-dotbar-window-status-format " #W "
-        set -g @tmux-dotbar-window-status-separator " • "
-        set -g @tmux-dotbar-maximized-icon "󰊓"
-        set -g @tmux-dotbar-show-maximized-icon-for-all-tabs false
-        set -g @plugin sainnhe/tmux-fzf
-        set -g @resurrect-strategy-vim "session"
-        set -g @resurrect-strategy-nvim "session"
-        set -g @resurrect-capture-pane-contents "on"
-        set -g @continuum-restore "on"
-        set -g @continuum-boot "on"
-        set -g @continuum-save-interval "10"
       '';
     };
   };
