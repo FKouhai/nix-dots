285d3dfb0a4ae9476e419b7330758e5194e6bd29
FKouhai <frandres00@gmail.com>
2026-06-10 22:01:23 +0200

    chore(hyprland): use noctalia systemd

--- modules/features/desktops/hyprland/config/exec.nix
+++ modules/features/desktops/hyprland/config/exec.nix
@@ -7,7 +7,6 @@
     "dbus-update-activation-environment --systemd --all"
     "add_record_player"
   ]
-  ++ lib.optionals (bar == "noctalia") [ "noctalia" ]
   ++ [
     "wl-paste --watch cliphist store &"
   ];
